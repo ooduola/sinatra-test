a078ef37daefb0fb63f66891e68031ca68987ce7
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-27 14:35:18 +0100

    Steps params

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -18,5 +18,6 @@ get '/name' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'> <img src='http://bit.ly/1eze8aE'> <div/>"
+  @sample_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
